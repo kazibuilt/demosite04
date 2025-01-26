6d4e6af57fe251413ecd960c9e01bc8d870db997
matthewkyleherren <99541432+matthewkyleherren@users.noreply.github.com>
2025-01-26 10:17:58 +0100

    dsgvds

---
+++ framerusercontent.com/sites/1wIBEVDiXphpRgdtZNT807/chunk-2ZBOGT5F.mjs
@@ -0,0 +1,12 @@
+import{a as t}from"./chunk-EO6IP77R.mjs";function n(e,a){return{bodyClassName:"framer-body-HaPfYsmQT",breakpoints:[{hash:"12sz1x",mediaQuery:"(min-width: 1100px)"},{hash:"ube0pv",mediaQuery:"(min-width: 810px) and (max-width: 1099px)"},{hash:"n3t41a",mediaQuery:"(max-width: 809px)"}],customHTMLHeadEnd:`<!-- Hotjar Tracking Code for Site 5254426 (name missing) -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5254426,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+<\/script>`,description:t(e,a).description,elements:{},robots:"max-image-preview:large",serializationId:"framer-3Y8rp",title:t(e,a).title||"Form",viewport:"width=device-width"}}var h=1,d={exports:{metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},default:{type:"function",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{n as a,h as b,d as c};
+//# sourceMappingURL=chunk-2ZBOGT5F.mjs.map
---
+++ framerusercontent.com/sites/1wIBEVDiXphpRgdtZNT807/chunk-EO6IP77R.mjs
@@ -0,0 +1,19 @@
+var r={UFPRtqdc_:{v0:"My Framer Site",v1:"Gemaakt in Framer"}};function o(a,e){for(;e;){let t=r[e.id];if(t){let n=t[a];if(n)return n}e=e.fallback}}function s(a,e){var t,n;return{customHTMLHeadEnd:`<style>
+    #p-hero{
+        height: 100svh !important;
+    }
+</style>`,customHTMLHeadStart:`<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2389663104710585');
+fbq('track', 'PageView');
+<\/script>
+<noscript></noscript>`,description:(t=o("v1",e))!==null&&t!==void 0?t:"Available: summer 2025. Pre-launch offer. We designed a camera that helps you to slow down, observe your surroundings and focus on the beauty of the present. Discover the joy of capturing life, one frame at a time. Because greatness, takes time.",favicon:"https://framerusercontent.com/assets/ntNHucVT47txoL8pN2qb7sj4f1U.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/0K9yw9ljVFMo5oMOYmZ6D7NQVY.jpg",title:(n=o("v0",e))!==null&&n!==void 0?n:"Analogue aF-1 | Just Point and Shoot | Pre-launch offer"}}export{s as a};
+//# sourceMappingURL=chunk-EO6IP77R.mjs.map
